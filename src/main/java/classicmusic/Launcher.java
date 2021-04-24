9a876e21a2d36f7550f95d3542ad289a746822f4
summetdev <summet.dev@gmail.com>
2021-04-24 18:23:23 +0300

    fixed classic music setting

--- src/main/java/classicmusic/Launcher.java
+++ src/main/java/classicmusic/Launcher.java
@@ -1,22 +1,25 @@
 package classicmusic;
 
+import arc.*;
+import arc.func.*;
 import mindustry.*;
 import mindustry.audio.*;
 import mindustry.mod.*;
 
 @SuppressWarnings("unused")
 public class Launcher extends Mod {
-    private final SoundControl original = new SoundControl(),
-            classic = new ClassicSoundControl();
+    private boolean enabled = Core.settings.getBool("classicmusic", true);
+    private SoundControl original = new SoundControl(), classic = new ClassicSoundControl();
+    private Boolc changed = (e) -> setSoundControl(e ? classic : original);
 
     public Launcher() {
-        setSoundControl(classic);
+        changed.get(enabled);
     }
 
     @Override
     public void init() {
         ClassicSoundtracks.load();
-        Vars.ui.settings.sound.checkPref("classicmusic", true, (b) -> setSoundControl(b ? classic : original));
+        Vars.ui.settings.sound.checkPref("classicmusic", enabled, changed);
     }
 
     private void setSoundControl(SoundControl control) {
