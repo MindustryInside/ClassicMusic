0ae7c1c10dbf9e174e30dd78bbd685182548ccd9
summetdev <summet.dev@gmail.com>
2021-05-13 23:12:49 +0300

    setting remove & bug fixes

--- src/main/java/classicmusic/Launcher.java
+++ src/main/java/classicmusic/Launcher.java
@@ -1,24 +1,49 @@
 package classicmusic;
 
 import arc.*;
+import arc.audio.*;
+import arc.struct.*;
 import mindustry.*;
 import mindustry.audio.*;
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
+
+import static mindustry.gen.Musics.*;
+import static classicmusic.ClassicSoundtracks.*;
 
 @SuppressWarnings("unused")
 public class Launcher extends Mod {
-    private final boolean enabled = Core.settings.getBool("classicmusic", true);
+    private Music lastRandom;
+    private Seq<Music> soundtracks;
+
+    public Launcher() {
+        Events.on(FileTreeInitEvent.class, e -> ClassicSoundtracks.load());
+    }
 
     @Override
     public void init() {
-        ClassicSoundtracks.load();
-        Vars.ui.settings.sound.checkPref("classicmusic", enabled, this::setSoundControl);
+        soundtracks = Seq.with(asFarAsItGets, fairyDust, hotTub, theShortestPath, weJustStarted, whileTrue);
 
-        setSoundControl(enabled);
+        menu = randomSoundtrack();
+        editor = randomSoundtrack();
+
+        game1 = randomSoundtrack();
+        game2 = randomSoundtrack();
+        game3 = randomSoundtrack();
+        game4 = randomSoundtrack();
+        game5 = randomSoundtrack();
+        game6 = randomSoundtrack();
+        game7 = randomSoundtrack();
+        game8 = randomSoundtrack();
+        game9 = randomSoundtrack();
+        boss1 = randomSoundtrack();
+        boss2 = randomSoundtrack();
+
+        // for SoundControl#reload
+        Vars.control.sound = new SoundControl();
     }
 
-    private void setSoundControl(boolean classic) {
-        Vars.control.sound.stop();
-        Vars.control.sound = classic ? new ClassicSoundControl() : new SoundControl();
+    private Music randomSoundtrack() {
+        return lastRandom = soundtracks.random(lastRandom);
     }
 }
